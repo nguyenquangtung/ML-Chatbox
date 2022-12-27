461407feaddfaf402ea6ad54a21bd9f6374630a5
foxthehuman <foxthehuman1900@gmail.com>
2022-12-27 21:11:01 +0700

    add check Intent

--- src/start.py
+++ src/start.py
@@ -1,17 +1,35 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
 from chatbox import get_response
+import subprocess
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 os.chdir(ROOT_DIR)
+
+# Check if intents change
+size = os.path.getsize("intents.json")
+check = ""
+
+with open('intent_size.txt', 'r') as temp_file:
+    check = temp_file.readline()
+
+if str(size) == check:
+    print(" No changes in Intents file")
+else:
+    open('intent_size.txt', 'w').close()
+    with open('intent_size.txt', 'w') as temp_file:
+        temp_file.write(str(size))
+    subprocess.run(["python training.py"])
+
+#########################################################
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.get("/chatbot")
+@app.get("/")
 def index_get():
     return render_template("base.html")
 
@@ -25,4 +43,4 @@ def userchat():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', debug=False)
